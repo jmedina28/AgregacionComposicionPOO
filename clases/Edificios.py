62f11f07bef3d3ee4371d7a8c38a6bf883f41eb9
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-18 11:19:04 +0100

    mejoro la complejidad del código

--- clases/Edificios.py
+++ clases/Edificios.py
@@ -5,20 +5,36 @@ class Edificio:
         self.empresa = empresa
         self.empleado = empleado
         self.estado = estado
-    
+
     def catastrofe(self):
         if self.ubicacion == "Nueva York":
             self.estado = "Destruido"
-        
+
 
 A = Edificio("Nueva York", "YooHoo!", "Sr. Martin", "Bueno")
 B = Edificio("Nueva York", "YooHoo!", "Sr. Salim", "Bueno")
 C = Edificio("Los Ángeles", "YooHoo!", "Sra. Xing", "Bueno")
 
+print("Ha elegido ejecutar el ejercicio 1, A continuación se le van a mostrar los datos de 3 edificaciones.")
+print("\nEl edificio A tiene las siguientes características:\n1. Ubicación: "+str(A.ubicacion) +
+      "\n2. Empresa: "+str(A.empresa)+"\n3. Empleado: "+str(A.empleado)+"\n4. Estado: "+str(A.estado))
+print("\nEl edificio B tiene las siguientes características:\n1. Ubicación: "+str(B.ubicacion) +
+      "\n2. Empresa: "+str(B.empresa)+"\n3. Empleado: "+str(B.empleado)+"\n4. Estado: "+str(B.estado))
+print("\nEl edificio C tiene las siguientes características:\n1. Ubicación: "+str(C.ubicacion) +
+      "\n2. Empresa: "+str(C.empresa)+"\n3. Empleado: "+str(C.empleado)+"\n4. Estado: "+str(C.estado))
 
-print(A.ubicacion, A.empresa, A.empleado, A.estado, sep=", ", end=".")
-print("\n")
-A.catastrofe()
 
-print(A.ubicacion, A.empresa, A.empleado, A.estado,sep=", ", end=".")
+variable = int(input(
+    "Si desea provocar una catástrofe en Nueva York pulse 1, en caso contrario pulse cualquier otro valor: "))
+if variable == 1:
+    Edificio.catastrofe(A)
+    Edificio.catastrofe(B)
+else:
+    exit()
 
+print("\nEl edificio A tiene las siguientes características:\n1. Ubicación: "+str(A.ubicacion) +
+      "\n2. Empresa: "+str(A.empresa)+"\n3. Empleado: "+str(A.empleado)+"\n4. Estado: "+str(A.estado))
+print("\nEl edificio B tiene las siguientes características:\n1. Ubicación: "+str(B.ubicacion) +
+      "\n2. Empresa: "+str(B.empresa)+"\n3. Empleado: "+str(B.empleado)+"\n4. Estado: "+str(B.estado))
+print("\nEl edificio C tiene las siguientes características:\n1. Ubicación: "+str(C.ubicacion) +
+      "\n2. Empresa: "+str(C.empresa)+"\n3. Empleado: "+str(C.empleado)+"\n4. Estado: "+str(C.estado))
