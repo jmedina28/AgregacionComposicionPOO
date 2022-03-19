7468b7cfd641ef89ec9c7963d7c68327f11988a3
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-03-19 12:40:35 +0100

    mejoro el código

--- clases/Edificios.py
+++ clases/Edificios.py
@@ -5,11 +5,18 @@ class Edificio:
         self.empresa = empresa
         self.empleado = empleado
         self.estado = estado
-
+    
     def catastrofe(self):
-        if self.ubicacion == "Nueva York":
             self.estado = "Destruido"
             self.empleado = "Fallecidos"
+    
+    def caracteristicas():
+        print("\nEl edificio A tiene las siguientes características:\n1. Ubicación: "+str(A.ubicacion) +
+      "\n2. Empresa: "+str(A.empresa)+"\n3. Empleado: "+str(A.empleado)+"\n4. Estado: "+str(A.estado))  
+        print("\nEl edificio B tiene las siguientes características:\n1. Ubicación: "+str(B.ubicacion) +
+      "\n2. Empresa: "+str(B.empresa)+"\n3. Empleado: "+str(B.empleado)+"\n4. Estado: "+str(B.estado))
+        print("\nEl edificio C tiene las siguientes características:\n1. Ubicación: "+str(C.ubicacion) +
+      "\n2. Empresa: "+str(C.empresa)+"\n3. Empleado: "+str(C.empleado)+"\n4. Estado: "+str(C.estado))
 
 
 A = Edificio("Nueva York", "YooHoo!", "Sr. Martin", "Bueno")
@@ -26,16 +33,23 @@ print("\nEl edificio C tiene las siguientes características:\n1. Ubicación: "+
 
 
 variable = int(input(
-    "Si desea provocar una catástrofe en Nueva York pulse 1, en caso contrario pulse cualquier otro valor: "))
+    "\nSi desea provocar una catástrofe en Nueva York pulse 1, si desea provocarla en Los Ángeles pulse cualquier otra tecla: "))
 if variable == 1:
-    Edificio.catastrofe(A)
-    Edificio.catastrofe(B)
+    Edificio.catastrofe(A),Edificio.catastrofe(B)
+    Edificio.caracteristicas()
+    variable2 = int(input("\nSi desea provocar el Apocalipsis pulse 1, en caso contrario pulse cualquier otro valor y se finalizará con la ejecución del programa: "))
+    if variable2 == 1:
+        Edificio.catastrofe(C)
+    else:
+        Edificio.caracteristicas()
+        exit()
 else:
-    exit()
-
-print("\nEl edificio A tiene las siguientes características:\n1. Ubicación: "+str(A.ubicacion) +
-      "\n2. Empresa: "+str(A.empresa)+"\n3. Empleado: "+str(A.empleado)+"\n4. Estado: "+str(A.estado))
-print("\nEl edificio B tiene las siguientes características:\n1. Ubicación: "+str(B.ubicacion) +
-      "\n2. Empresa: "+str(B.empresa)+"\n3. Empleado: "+str(B.empleado)+"\n4. Estado: "+str(B.estado))
-print("\nEl edificio C tiene las siguientes características:\n1. Ubicación: "+str(C.ubicacion) +
-      "\n2. Empresa: "+str(C.empresa)+"\n3. Empleado: "+str(C.empleado)+"\n4. Estado: "+str(C.estado))
+    Edificio.catastrofe(C)
+    Edificio.caracteristicas()
+    variable2 = int(input("\nSi desea provocar el Apocalipsis pulse 1, en caso contrario pulse cualquier otro valor y se finalizará con la ejecución del programa: "))
+    if variable2 == 1:
+        Edificio.catastrofe(A),Edificio.catastrofe(B)
+    else:
+        Edificio.caracteristicas()
+        exit()
+Edificio.caracteristicas()
